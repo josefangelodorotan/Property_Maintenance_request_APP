f13e2232ebfd5a771348b9c0961ecd1f582c521a
Jan Josef Angelo Dorotan <josefangelo.dorotan@gmail.com>
2024-03-02 13:12:34 +0300

    adding functions

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,50 @@
+class TasksController < ApplicationController
+  before_action :set_category
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @task = @category.tasks.build
+  end
+
+  def show
+
+  end
+
+  def create
+    @task = @category.tasks.build(task_params)
+    if @task.save
+      redirect_to @category, notice: 'Task was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @task.update(task_params)
+      redirect_to @category, notice: 'Task was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to @category, notice: 'Task was successfully destroyed.'
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:category_id])
+  end
+
+  def set_task
+    @task = @category.tasks.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :description, :category_id)
+  end
+end
